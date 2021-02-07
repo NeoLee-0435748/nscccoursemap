66e04e22239e942badc3b4fd235b2710f0e0f037
neo.lee <w0435748@nscc.ca>
2021-02-06 22:49:13 -0400

    Modify Indentity

--- Migrations/NsccCourseMap_NeoIdentityDb/NsccCourseMap_NeoIdentityDbContextModelSnapshot.cs
+++ Migrations/NsccCourseMap_NeoIdentityDb/NsccCourseMap_NeoIdentityDbContextModelSnapshot.cs
@@ -15,7 +15,7 @@ namespace NsccCourseMap_Neo.Migrations.NsccCourseMap_NeoIdentityDb
         {
 #pragma warning disable 612, 618
             modelBuilder
-                .HasAnnotation("ProductVersion", "3.1.3")
+                .HasAnnotation("ProductVersion", "3.1.0")
                 .HasAnnotation("Relational:MaxIdentifierLength", 128)
                 .HasAnnotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn);
 
